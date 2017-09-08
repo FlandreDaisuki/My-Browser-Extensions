fc5125c96b23aea79ce27150f5430b1c2c83a956
FlandreDaisuki <vbnm123c@gmail.com>
2017-09-09 03:17:49 +0800

    🛠 Fix load event
    https://stackoverflow.com/questions/36438721/

--- userscripts/PixivAuto10.user.js
+++ userscripts/PixivAuto10.user.js
@@ -3,7 +3,7 @@
 // @description Rating 10 stars / smile automatically
 // @namespace   https://github.com/FlandreDaisuki
 // @include     *://www.pixiv.net/member_illust.php?*
-// @version     2.1.2
+// @version     2.1.3
 // @icon        http://i.imgur.com/cRWoMhw.png
 // @require     https://cdnjs.cloudflare.com/ajax/libs/axios/0.15.3/axios.min.js
 // @grant       none
@@ -12,26 +12,23 @@
 // @noframes
 // ==/UserScript==
 
-window.addEventListener('load', function() {
-	// Chrome compatible
-	if (!document.getElementsByClassName('rated').length) {
-		const tt = document.querySelector('input[name="tt"]').value;
-		const i_id = pixiv.context.illustId;
-		const u_id = pixiv.context.userId;
-		axios({
-			method: 'POST',
-			url: '/rpc_rating.php',
-			data: `mode=save&i_id=${i_id}&u_id=${u_id}&qr=0&score=10&tt=${tt}`,
-			headers: {
-				'Content-type':
-					'application/x-www-form-urlencoded; charset=UTF-8',
-			},
-		}).then(() => {
-			$('.js-nice-button')
-				.addClass('rated')
-				.off()
-				.find('.smile')
-				.addClass('active');
-		});
-	}
-});
+// Chrome compatible
+if (!document.getElementsByClassName('rated').length) {
+	const tt = document.querySelector('input[name="tt"]').value;
+	const i_id = pixiv.context.illustId;
+	const u_id = pixiv.context.userId;
+	axios({
+		method: 'POST',
+		url: '/rpc_rating.php',
+		data: `mode=save&i_id=${i_id}&u_id=${u_id}&qr=0&score=10&tt=${tt}`,
+		headers: {
+			'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+		},
+	}).then(() => {
+		$('.js-nice-button')
+			.addClass('rated')
+			.off()
+			.find('.smile')
+			.addClass('active');
+	});
+}
