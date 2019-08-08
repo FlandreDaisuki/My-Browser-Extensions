0cd87e17116e3527cc30c879a29ee4b72a5ab431
Chun-Hao Lien <vbnm123c@gmail.com>
2019-08-08 14:02:09 +0800

    Update F❍ck-Facebook-Sponsor.user.js

--- userscripts/F❍ck-Facebook-Sponsor.user.js
+++ userscripts/F❍ck-Facebook-Sponsor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         F❍ck Facebook Sponsor
 // @namespace    https://github.com/FlandreDaisuki
-// @version      0.4.1
+// @version      0.5.0
 // @description  Remove Facebook sponsor feeds
 // @author       FlandreDaisuki
 // @match        https://www.facebook.com/*
@@ -15,11 +15,16 @@
 const rules = [
   '[id^="feed_subtitle_"]',
   '[id^="feedsub_title_"]',
+  '[id^="fb-header-info-"]',
 ];
+
+let counter = 0;
 
 sentinel.on(rules.join(','), (el) => {
   const p = el.id.match(/:/g); // pattern
   if (p && p.length > 3) {
     el.closest('[id^="hyperfeed_story_id"]').remove();
+    // You can remove 2 shashes in front of following code to show how many feeds have been removed.
+    // console.debug(`F❍ck up ${++counter} sponsor feeds!`);
   }
 });
