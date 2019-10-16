4eb129070963ad3d745a0b60e01291af836c151b
FlandreDaisuki <vbnm123c@gmail.com>
2019-10-16 23:05:16 +0800

    Update javbus.waterfall

--- userscripts/javbus.waterfall.user.js
+++ userscripts/javbus.waterfall.user.js
@@ -3,196 +3,155 @@
 // @description Infinite scroll @ javbus.com
 // @namespace   https://github.com/FlandreDaisuki
 // @include     *://*.javbus.*/*
-// @version     2017.11.02
+// @version     2019.10.16
 // @grant       none
+// @license     MIT
+// @noframes
 // ==/UserScript==
+
+/* global jQuery */
+
+// Configuration
+const FETCH_TRIG = '400px';
+const COL_CNT = 4;
+const IS_DENSE = false;
+
+// Utilities
 const $ = jQuery;
 
-class Lock {
-	constructor(d = false) {
-		this.locked = d;
-	}
-	lock() {
-		this.locked = true;
-	}
-	unlock() {
-		this.locked = false;
-	}
-}
+const global = {
+  pageCount: 0,
+  itemCount: 0,
+  parent: $('#waterfall'),
+  columnParents: Array(COL_CNT).fill().map(() => $('<div class="column">')),
+  columnHeights: Array(COL_CNT).fill(0),
+  nextURL: location.href,
+  locked: false,
+  baseURI: location.origin,
+  selector: {
+    next: 'a#next',
+    item: '.item',
+  },
+};
 
-class Waterfall {
-	constructor(selectorcfg = {}) {
-		this.lock = new Lock();
-		this.baseURI = this.getBaseURI();
-		this.selector = {
-			next: 'a.next',
-			item: '',
-			cont: '', //container
-			pagi: '.pagination',
-		};
-		Object.assign(this.selector, selectorcfg);
-		this.pagegen = this.fetchSync(location.href);
-		this.anchor = $(this.selector.pagi)[0];
-		this._count = 0;
-		this._1func = function(cont, elems) {
-			cont.empty().append(elems);
-		};
-		this._2func = function(cont, elems) {
-			cont.append(elems);
-		};
+const addStyle = (styleStr) => {
+  $('head').append(`<style>${styleStr}</style>`);
+};
 
-		if ($(this.selector.item).length) {
-			document.addEventListener('scroll', this.scroll.bind(this));
-			document.addEventListener('wheel', this.wheel.bind(this));
-			this.appendElems(this._1func);
-		}
-	}
+const getNextURL = (href) => {
+  const a = document.createElement('a');
+  a.href = href;
+  return `${global.baseURI}${a.pathname}${a.search}`;
+};
 
-	getBaseURI() {
-		let _ = location;
-		return `${_.protocol}//${_.hostname}${_.port && `:${_.port}`}`;
-	}
+const fetchURL = async(url) => {
+  console.log(`fetchUrl = ${url}`);
 
-	getNextURL(href) {
-		let a = document.createElement('a');
-		a.href = href;
-		return `${this.baseURI}${a.pathname}${a.search}`;
-	}
+  const resp = await fetch(url, { credentials: 'same-origin' });
+  const html = await resp.text();
+  const doc = new DOMParser().parseFromString(html, 'text/html');
 
-	fetchURL(url) {
-		console.log(`fetchUrl = ${url}`);
-		const fetchwithcookie = fetch(url, { credentials: 'same-origin' });
-		return fetchwithcookie
-			.then(response => response.text())
-			.then(html => new DOMParser().parseFromString(html, 'text/html'))
-			.then(doc => {
-				let $doc = $(doc);
-				let href = $doc.find(this.selector.next).attr('href');
-				let nextURL = href ? this.getNextURL(href) : undefined;
-				let elems = $doc.find(this.selector.item);
+  const $doc = $(doc);
 
-				return {
-					nextURL,
-					elems,
-				};
-			});
-	}
+  const href = $doc.find(global.selector.next).attr('href');
+  const nextURL = href ? getNextURL(href) : null;
+  const elems = $doc.find(global.selector.item);
 
-	*fetchSync(urli) {
-		let url = urli;
-		do {
-			yield new Promise((resolve, reject) => {
-				if (this.lock.locked) {
-					reject();
-				} else {
-					this.lock.lock();
-					resolve();
-				}
-			})
-				.then(() => {
-					return this.fetchURL(url).then(info => {
-						url = info.nextURL;
-						return info.elems;
-					});
-				})
-				.then(elems => {
-					this.lock.unlock();
-					return elems;
-				})
-				.catch(err => {
-					// Locked!
-				});
-		} while (url);
-	}
+  return {
+    nextURL,
+    elems,
+  };
+};
 
-	appendElems() {
-		let nextpage = this.pagegen.next();
-		if (!nextpage.done) {
-			nextpage.value.then(elems => {
-				const cb = this._count === 0 ? this._1func : this._2func;
-				cb($(this.selector.cont), elems);
-				this._count += 1;
-			});
-		}
-		return nextpage.done;
-	}
+const intersectionObserverOptions = {
+  rootMargin: `0px 0px ${FETCH_TRIG} 0px`,
+  threshold: Array(5).fill().map((_, index, arr) => index / arr.length),
+};
 
-	end() {
-		console.info('The End');
-		document.removeEventListener('scroll', this.scroll.bind(this));
-		document.removeEventListener('wheel', this.wheel.bind(this));
-		$(this.anchor).replaceWith($(`<h1>The End</h1>`));
-	}
+const intersectionObserver = new IntersectionObserver(async() => {
+  if (global.locked) { return; }
+  global.locked = true;
+  const { nextURL, elems } = await fetchURL(global.nextURL);
 
-	static reachBottom(elem, limit) {
-		return elem.getBoundingClientRect().top - $(window).height() < limit;
-	}
-
-	scroll() {
-		if (
-			Waterfall.reachBottom(this.anchor, 500) &&
-			this.appendElems(this._2func)
-		) {
-			this.end();
-		}
-	}
-
-	wheel() {
-		if (
-			Waterfall.reachBottom(this.anchor, 1000) &&
-			this.appendElems(this._2func)
-		) {
-			this.end();
-		}
-	}
-
-	setFirstCallback(f) {
-		this._1func = f;
-	}
-
-	setSecondCallback(f) {
-		this._2func = f;
-	}
-}
-
-const w = new Waterfall({
-	next: 'a#next',
-	item: '.item',
-	cont: '#waterfall',
-});
-
-w.setSecondCallback(function(cont, elems) {
-	if (location.pathname.includes('/star/')) {
-		cont.append(elems.slice(1));
-	} else {
-		cont.append(elems);
-	}
-});
-
-function addStyle(styleStr) {
-	$('head').append(`<style>${styleStr}</style>`);
-}
-
-addStyle(`
-  #waterfall {
-    height: initial !important;
-    width: initial !important;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
+  if (global.pageCount === 0) {
+    global.parent.empty().append(global.columnParents);
   }
-  #waterfall .item.item {
-    position: relative !important;
-    top: initial !important;
-    left: initial !important;
-    float: none;
-    flex: 25%;
+
+  const items = elems.toArray();
+  if (global.pageCount > 0 && location.pathname.includes('/star/')) {
+    items.shift();
   }
-  #waterfall .movie-box,
-  #waterfall .avatar-box {
-    width: initial !important;
-    display: flex;
+
+  for (const item of items) {
+    if (IS_DENSE) {
+      const idx = global.columnHeights.indexOf(Math.min(...global.columnHeights));
+      global.columnParents[idx].append(item);
+      global.columnHeights[idx] += item.clientHeight;
+    } else {
+      global.columnParents[global.itemCount % COL_CNT].append(item);
+    }
+
+    global.itemCount += 1;
   }
-  #waterfall .movie-box .photo-frame {
-    overflow: visible;
-  }`);
+
+  // finally
+  global.pageCount += 1;
+  global.nextURL = nextURL;
+  global.locked = false;
+
+  if (!global.nextURL) {
+    console.info('The End');
+    global.parent.after($('<h1 id="end">The End</h1>'));
+    intersectionObserver.disconnect();
+    return;
+  }
+}, intersectionObserverOptions);
+
+intersectionObserver.observe($('footer').get(0));
+
+const SHARED_STYLE = `
+#waterfall {
+  height: initial !important;
+  width: initial !important;
+  display: flex;
+}
+#waterfall .item.item {
+  position: relative !important;
+  top: initial !important;
+  left: initial !important;
+  float: none;
+}
+h1#end {
+  text-align: center;
+}`;
+
+const GRID_STYLE = `
+#waterfall > .column {
+  flex: 1;
+}
+#waterfall .movie-box,
+#waterfall .avatar-box {
+  width: initial !important;
+  display: flex;
+}
+#waterfall .movie-box .photo-frame {
+  overflow: visible;
+}`;
+
+const DENSE_STYLE = `
+#waterfall {
+  justify-content: center;
+}
+#waterfall > .column {
+  display: flex;
+  flex-direction: column;
+}
+#waterfall .item.item {
+  position: relative !important;
+  top: initial !important;
+  left: initial !important;
+  float: none;
+}`;
+
+addStyle(SHARED_STYLE);
+addStyle(IS_DENSE ? DENSE_STYLE : GRID_STYLE);
