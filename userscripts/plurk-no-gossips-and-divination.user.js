5ad8b7fe8355bef625aa793c9ac3a1f00935de71
FlandreDaisuki <vbnm123c@gmail.com>
2019-12-19 23:48:03 +0800

    Update plurk-no-gossips-and-divination

--- userscripts/plurk-no-gossips-and-divination.user.js
+++ userscripts/plurk-no-gossips-and-divination.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         噗浪不語怪力亂神
 // @name:en      Plurk: No Gossips and Divination
-// @description    隱藏各式各樣問神、心裡測驗、奇怪的跟風
+// @description    隱藏各式各樣問神、心理測驗、奇怪的跟風
 // @description:en Hide and mute any gossip and divination
 // @namespace    https://github.com/FlandreDaisuki
-// @version      0.1.1
+// @version      0.1.3
 // @author       FlandreDaisuki
 // @match        https://www.plurk.com/*
 // @require      https://unpkg.com/sentinel-js@0.0.5/dist/sentinel.js
@@ -23,7 +23,7 @@ const hideEl = (el) => {
 
 const mute = (plurk) => {
   const toMute = plurk.querySelector('.mute-off');
-  if(toMute) {
+  if (toMute) {
     toMute.click();
   }
 };
@@ -35,29 +35,36 @@ sentinel.on('#timeline_cnt > .block_cnt > .plurk', (plurk) => {
   // 例外條件
   const exceptConditions = [
     // 自己
-    plurk.textContent.includes(GLOBAL.session_user.display_name)
+    plurk.textContent.includes(GLOBAL.session_user.display_name),
   ];
 
   // 表情符號網址
   const emos = [
-    '3c06d451efef7089cb388307a1af57a1_w44_h17.png',
+    '3c06d451efef7089cb388307a1af57a1_w44_h17.png', // 跟風
+    '79269cb907d5e8f5a2737586c2693389_w44_h17.gif', // 跟風
+  ];
+
+  // 黑名單網址
+  const blackList = [
+    '//shindanmaker.com',
   ];
 
   // 隱藏條件
   const hideConditions = [
+    [...plurk.querySelectorAll('.ex_link')].some((a) => blackList.some((black) => a.getAttribute('href').includes(black))),
     // [問] 神
     qualifierEls.filter((q) => q.classList.contains('q_asks')).length && content.match(/^神\s*/g),
     // 關鍵字
-    content.match(/(跟風|心測|心理測驗)/g),
+    content.match(/(跟風|心測|心理測驗|性向測驗)/g),
     // 表情符號
     plurk.querySelector(emos.map((emo) => `img[src="https://emos.plurk.com/${emo}"]`).join(',')),
   ];
 
-  if(exceptConditions.some(Boolean)) {
+  if (exceptConditions.some(Boolean)) {
     return;
   }
 
-  if(hideConditions.some(Boolean)) {
+  if (hideConditions.some(Boolean)) {
     hideEl(plurk);
     mute(plurk);
   }
