d0514283ae699af2b121b9fde726288b0991733d
FlandreDaisuki <vbnm123c@gmail.com>
2021-03-07 00:38:30 +0800

    Update 乾淨ㄉ原價屋

--- userscripts/clean-coolpc-evaluate.user.js
+++ userscripts/clean-coolpc-evaluate.user.js
@@ -2,41 +2,95 @@
 // @name         乾淨ㄉ原價屋
 // @namespace    https://github.com/FlandreDaisuki
 // @description  讓我好好的選零件…
-// @version      0.1
+// @version      2021.03.07
 // @author       FlandreDaisuki
 // @match        http://www.coolpc.com.tw/evaluate.php
 // @require      https://unpkg.com/sentinel-js@0.0.5/dist/sentinel.js
-// @noiframes
-// @grant        unsafeWindow
+// @noframes
 // @license      MIT
 // ==/UserScript==
 
 // 推薦搭配 AdGuard Annoyances 過濾清單
 
-const noop = () => {};
+/* cSpell:ignore vwpro mesg Ttitle Fitem */
+/* global sentinel */
+
 const $ = (s) => document.querySelector(s);
-const $$ = (s) => [...document.querySelectorAll(s)];
-const removeEl = (el) => { el.remove(); }
 
-unsafeWindow.getData = noop;
-unsafeWindow.SkipMsg = () => unsafeWindow.Gauze(2);
-
-unsafeWindow._ViewPro = unsafeWindow.ViewPro;
-unsafeWindow.ViewPro = (a, b) => {
-  if(!a) { return alert('請先【產生擷取檔】'); }
-  if(!b) { open(a); }
-  unsafeWindow._ViewPro(a, b);
-}
-
+// 取消鎖右鍵
 document.body.removeAttribute('oncontextmenu');
 document.body.removeAttribute('onselectstart');
-document.body.removeAttribute('onkeyup');
 
-// 上廣告
-const ad = $('img[src*="/ad"]');
-if(ad) {
-  ad.closest('table').remove();
+// 總金額預覽移到上面
+sentinel.on('#fDiv', (floatingBar) => {
+  sentinel.off('#fDiv');
+  floatingBar.remove();
+  $('#know').insertAdjacentElement('afterend', floatingBar);
+});
+
+// 保留輸出圖片功能
+sentinel.on('#vwpro', () => {
+  $('#doc').classList.add('zTop');
+});
+
+sentinel.on('button[onclick="Gauze(2)"]', (imageExportCancelBtnEl) => {
+  sentinel.off('button[onclick="Gauze(2)"]');
+  imageExportCancelBtnEl.addEventListener('click', () => {
+    $('#doc').classList.remove('zTop');
+  });
+});
+
+// 刪除無用的 iframe
+const iframeSelectors = [
+  'iframe[src^="/home"]',
+  'iframe[src$="eval-mesg.php"]',
+];
+
+sentinel.on(String(iframeSelectors), (iframe) => {
+  iframe.remove();
+});
+
+// 調整樣式
+const styleEl = document.createElement('style');
+document.body.appendChild(styleEl);
+styleEl.textContent = `
+body {
+  overflow: auto !important;
 }
-
-// iframe
-sentinel.on('iframe:not([src="eval-save.php"])', removeEl);
+#Psu,
+#hid,
+#Ttitle > tbody > tr:nth-of-type(1) > td:nth-of-type(2),
+#Ttitle > tbody > tr:nth-of-type(2) {
+  display: none !important;
+}
+#doc {
+  z-index: -1 !important;
+}
+form[action="eva-excel.php"] {
+  position: relative;
+}
+#Ttitle > tbody > tr:first-of-type > td:first-of-type {
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+}
+#fDiv {
+  position: relative;
+  display: flex;
+  transform: initial;
+  left: inherit !important
+}
+#ftb {
+  margin: 0;
+  border: 2px solid black;
+}
+#Fitem {
+  position: relative !important;
+}
+#doc.zTop {
+  z-index: 20 !important;
+  background: rgba(0, 0, 0, 0.8);
+}
+#MatchArea {
+  position: absolute;
+  top: 100%;
+}`;
