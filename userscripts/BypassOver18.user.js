5525024aa5a8a83d4edc6432a3a0e1fa839ec24c
FlandreDaisuki <vbnm123c@gmail.com>
2022-05-28 22:56:36 +0800

    Update BypassOver18

--- userscripts/BypassOver18.user.js
+++ userscripts/BypassOver18.user.js
@@ -2,7 +2,7 @@
 // @name         Bypass 18+
 // @description  Bypass 18+ checking forever
 // @namespace    https://github.com/FlandreDaisuki
-// @version      1.1.1
+// @version      1.2.0
 // @author       FlandreDaisuki
 // @match        *://www.amazon.co.jp/*
 // @match        *://gyutto.com/*
@@ -12,6 +12,7 @@
 // @match        *://www.melonbooks.co.jp/*
 // @match        *://www.suruga-ya.jp/*
 // @match        *://www.javlibrary.com/*
+// @include      *://javdb*.com/*
 // @run-at       document-start
 // @grant        none
 // @noframes
@@ -66,39 +67,51 @@ class CookieBuilder {
       return c;
     }, new Map());
 
-  switch (location.host) {
-  case 'www.amazon.co.jp':
-  {
+  const host = location.host;
+  const any = (...args) => args.some(Boolean);
+
+  if (host === 'www.amazon.co.jp') {
     const t = cookieMap.get('csm-hit');
     if (t) {
       bake('csm-hit', t)['⏰'];
     }
-    break;
+    return;
   }
-  case 'gyutto.com':
+
+  if (host === 'gyutto.com') {
     bake('adult_check_flag', 1).domain('gyutto.com')['⏰'];
     bake('user_agent_flag', 1).domain('gyutto.com')['⏰'];
-    break;
-  case 'www.dlsite.com':
+    return;
+  }
+
+  if (host === 'www.dlsite.com') {
     bake('adultchecked', 1).domain('dlsite.com')['⏰'];
     bake('uniqid', '0.00000000000').domain('dlsite.com')['⏰'];
-    break;
-  case 'ec.toranoana.jp':
+    return;
+  }
+
+  if (host === 'ec.toranoana.jp') {
     bake('adflg', 0).domain('ec.toranoana.jp')['⏰'];
-    break;
-  case 'www.ptt.cc':
-    bake('over18', 1)['⏰'];
-    break;
-  case 'www.melonbooks.co.jp':
+    return;
+  }
+
+  if (host === 'www.melonbooks.co.jp') {
     bake('AUTH_ADULT', 1)['⏰'];
-    break;
-  case 'www.suruga-ya.jp':
+    return;
+  }
+
+  if (host === 'www.suruga-ya.jp') {
     bake('adult', 1)['⏰'];
-    break;
-  case 'www.javlibrary.com':
+    return;
+  }
+
+  if (host === 'www.javlibrary.com') {
     bake('over18', 18)['⏰'];
-    break;
-  default:
-    break;
+    return;
+  }
+
+  if (any(host === 'www.ptt.cc', host.startsWith('javdb'))) {
+    bake('over18', 1)['⏰'];
+    return;
   }
 })();
