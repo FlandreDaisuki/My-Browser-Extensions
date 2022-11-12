d28017ca54cc02a995ba5e97e063d13f9328b57b
FlandreDaisuki <vbnm123c@gmail.com>
2022-11-13 03:14:56 +0800

    Update configs

---
+++ userscripts/helpers/common.d.ts
@@ -0,0 +1,54 @@
+export function noop(): void;
+
+// #region dom
+export function $find<K extends keyof HTMLElementTagNameMap>(el: HTMLElement, selectors: K): HTMLElementTagNameMap[K] | null;
+export function $findAll(el: HTMLElement, selectors: string): HTMLElement[];
+export function $(selectors: string): HTMLElement | null;
+export function $$(selectors: string): HTMLElement[];
+export function $html(htmlText: string): HTMLElement;
+export function $style(stylesheet: string): HTMLStyleElement;
+export function $el<K extends string>(
+  tag: K,
+  attr?: Record<string, unknown> | string,
+  cb?: (el: K extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[K] : HTMLElement) => void
+): K extends keyof HTMLElementTagNameMap ? HTMLElementTagNameMap[K] : HTMLElement;
+// #endregion dom
+
+export function $getValue<T, D>(key: string, defaultValue: D): Promise<T|D>;
+export function $setValue<T>(key: string, value: T): Promise<void>;
+export function $getResourceText(key: string): string;
+export function $xhr(details: GmXhrDetails): { abort: () => void };
+
+interface GmXhrDetails {
+  url: string | URL;
+  method: 'GET' | 'HEAD' | 'POST';
+  headers?: Record<string, unknown>;
+  data?: string;
+  binary?: boolean;
+  /** milliseconds */
+  timeout?: number;
+  /**  This object will also be the context property of the #Response Object. */
+  context?: Record<string, unknown>;
+  responseType?: 'arraybuffer' | 'blob' | 'json';
+  overrideMimeType?: string;
+  user?: string;
+  password?: string;
+  onabort?(response: GmXhrResponse): void;
+  onerror?(response: GmXhrResponse): void;
+  onprogress?(response: GmXhrResponse): void;
+  onreadystatechange?(response: GmXhrResponse): void;
+  ontimeout?(response: GmXhrResponse): void;
+  onload?(response: GmXhrResponse): void;
+}
+
+interface GmXhrResponse {
+  finalUrl: string;
+  readyState: XMLHttpRequest['readyState'];
+  status: XMLHttpRequest['status'];
+  statusText: XMLHttpRequest['statusText'];
+  responseHeaders: string;
+  response: XMLHttpRequest['response'];
+  responseXML: XMLHttpRequest['responseXML'];
+  responseText: XMLHttpRequest['responseText'];
+  context: Record<string, unknown>;
+}
--- userscripts/helpers/common.js
+++ userscripts/helpers/common.js
@@ -1,18 +1,10 @@
 export const noop = () => {};
 
-/** @type {(el: HTMLElement, selectors: string) => HTMLElement | null} */
 export const $find = (el, selectors) => el.querySelector(selectors);
-
-/** @type {(el: HTMLElement, selectors: string) => HTMLElement[]} */
 export const $findAll = (el, selectors) => Array.from(el.querySelectorAll(selectors));
-
-/** @type {(selectors: string) => HTMLElement | null} */
 export const $ = (selectors) => document.querySelector(selectors);
-
-/** @type {(selectors: string) => HTMLElement[]} */
 export const $$ = (selectors) => Array.from(document.querySelectorAll(selectors));
 
-/** @type {(tag: string, attr: Record<string, unknown>, cb: (el: HTMLElement) => void) => HTMLElement} */
 export const $el = (tag, attr = {}, cb = noop) => {
   const el = document.createElement(tag);
   if (typeof (attr) === 'string') {
@@ -25,7 +17,6 @@ export const $el = (tag, attr = {}, cb = noop) => {
   return el;
 };
 
-/** @type {(htmlText: string) => HTMLElement} */
 export const $html = (htmlText) => {
   const tmpEl = $el('div');
   tmpEl.innerHTML = htmlText;
@@ -35,7 +26,6 @@ export const $html = (htmlText) => {
 export const $style = (stylesheet) => $el('style', stylesheet, (el) => document.head.appendChild(el));
 
 export const $getValue = async(key, defaultValue) => {
-  /* global globalThis */
   if (globalThis.GM_getValue) {
     return globalThis.GM_getValue(key, defaultValue);
   }
@@ -60,4 +50,9 @@ export const $getResourceText = (key) => {
   else if (globalThis.GM.getResourceText){
     return globalThis.GM.getResourceText( key );
   }
+};
+
+export const $xhr = (details) => {
+  const xhrFn = globalThis.GM_xmlhttpRequest ?? GM.xmlhttpRequest;
+  return xhrFn(details);
 };
